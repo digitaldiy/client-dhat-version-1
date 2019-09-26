40f60b3cd9db0142992fe01b18d20320fed01b89
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2019-09-26 15:24:32 +0100

    div relocation

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,7 @@
         <div class="container">
             <span v-html="settings.footer" />
               <vs-button @click="popupActivo=true" color="primary" type="border">Open Default popup</vs-button>
-        </div>
-    </footer>
-     <div class="centerx">
+                <div class="centerx">
     <vs-popup class="holamundo"  title="Lorem ipsum dolor sit amet" :active.sync="popupActivo">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -13,6 +11,9 @@
       </p>
     </vs-popup>
   </div>
+        </div>
+    </footer>
+   
 </template>
 
 
