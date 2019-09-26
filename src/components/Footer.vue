fdf7199000d2231bc27ba1efc6f43b83a5e0ce72
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2019-09-26 14:28:09 +0100

    popup?

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,37 +1,33 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <span v-html="settings.footer" />
+              <vs-button @click="popupActivo=true" color="primary" type="border">Open Default popup</vs-button>
+        </div>
+    </footer>
+     <div class="centerx">
+    <vs-popup class="holamundo"  title="Lorem ipsum dolor sit amet" :active.sync="popupActivo">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      </p>
+    </vs-popup>
+  </div>
+</template>
+
+
+<script>
 
 import Vue from 'vue'
 import Vuesax from 'vuesax'
 
 import 'vuesax/dist/vuesax.css' //Vuesax styles
-Vue.use(Vuesax, {
-  // options here
-})
 
-
-
-
-<template>
-    <footer class="footer">
-        <div class="container">
-            <span v-html="settings.footer" />
-            Vue.use(Vuesax, {
-   <vs-list>
-    <vs-list-item title="One text"></vs-list-item>
-    <vs-list-item title="Another text" subtitle="A little text"></vs-list-item>
-    <vs-list-item title="Some more text"></vs-list-item>
-    <vs-list-item title="Even more text" subtitle="Another little text"></vs-list-item>
-  </vs-list>
-})
-        
-        </div>
-    </footer>
-</template>
-
-<script>
 export default {
     data() {
         return {
             settings: require("../../data/theme.json")
+              popupActivo:false,
         }
     }
 }
