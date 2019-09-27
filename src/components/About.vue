5d66ef792af4ba35eab96f762d7c0b5dc02ecb70
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2019-09-27 13:50:50 +0100

    fix push

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
    <vs-list>
-    <vs-list-item title="One text">content.opening</vs-list-item>
+    <vs-list-item title="One text">"content.opening"</vs-list-item>
     <vs-list-item title="Another text" subtitle="A little text"></vs-list-item>
     <vs-list-item title="Some more text"></vs-list-item>
     <vs-list-item title="Even more text" subtitle="Another little text"></vs-list-item>
