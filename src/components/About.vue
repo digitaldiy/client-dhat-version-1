720cd26095f9a4488ac6e52151419ade572487c2
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2019-09-27 14:20:00 +0100

    about alt

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,8 @@
 <template>
-<vs-row
-  vs-align="center"
-  vs-type="flex" vs-justify="space-around" vs-w="12">
-  <vs-col :key="index" v-for="col,index in 4" vs-type="flex" vs-justify="center" vs-align="center" vs-w="2">
-   <vs-list >
-    <vs-list-item title="One text">{{ content.opening }}</vs-list-item>
-    <vs-list-item title="Another text" subtitle="A little text">{{ content.opening }}</vs-list-item>
-    <vs-list-item title="Some more text">{{ content.opening }}</vs-list-item>
+   <vs-list vs-align="center">
     <vs-list-item title="Even more text" subtitle="Another little text">{{ content.opening }}</vs-list-item>
   </vs-list>
-  </vs-col>
-  </vs-row>
+ 
 </template>
 
 <script>
