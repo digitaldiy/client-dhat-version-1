67ab8c2a941d341d83b3259fc13a568eb743ebfb
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2020-01-17 16:29:08 +0000

    right one

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -2,7 +2,7 @@
 <div>
      <h4 class="md-display-4 center">What are people saying</h4>
     <md-divider class="md-inset"></md-divider>
-<div id='ndrsl-5dcc55f42cba5b627688423e' class='ndrsl-widget'></div>
+<div id='ndrsl-5dfbad0887d3740573fd79e2' class='ndrsl-widget'></div>
 </div>
 </template>
 
