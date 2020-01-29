82e84bb5aa263ec14400db5e4878aea88da1875d
DDN <digitaldiyninja@gmail.com>
2020-01-29 14:43:57 +0100

    speed boost

--- src/pages/Booking.vue
+++ src/pages/Booking.vue
@@ -6,7 +6,7 @@
     
     <md-layout md-flex-xsmall="100" md-flex-small="100" md-flex-medium="100" md-flex-large="100" md-flex-xlarge="100">
 
- <div class="powr-contact-form" id="0c1e130f_1579201566"></div>
+<iframe src="https://www.powr.io/plugins/contact-form/view/22657594" style="width:100%;" height="529px" frameborder="0"/>
 
 
     </md-layout>
